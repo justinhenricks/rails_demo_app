fe2241ba62e4c1ecd460b5472c521790e2c45807
Justin Henricks <justinhenricks@gmail.com>
2014-04-10 15:22:18 -0400

    Home Page!

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def pageTitle(title)
+    base_title = "Ruby on Rails Demonstration Application"
+
+    if title.empty?
+      base_title
+    else
+      "#{base_title} | #{title}" # #{ } is essentially string concatenation for variables in ruby
+    end
+  end
+
 end
---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+  describe "Home page" do
+
+    it "should have the content 'Sample App'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Home Page') #page is built into
+    end
+  end
+end
